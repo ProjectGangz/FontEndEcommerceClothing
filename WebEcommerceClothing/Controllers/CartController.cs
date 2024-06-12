de8a8f8b1e66c139534215135870aec70cfe9a62
lovefamilyviet203 <huyviet181@gmail.com>
2024-06-12 21:43:26 +0700

    Them cart, productdetails

---
+++ WebEcommerceClothing/Controllers/CartController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebEcommerceClothing.Controllers
+{
+    public class CartController : Controller
+    {
+        public IActionResult ShoppingCart()
+        {
+            return View();
+        }
+    }
+}
